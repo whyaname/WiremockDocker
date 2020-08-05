2a435abe0986ae6791e7e73d0362053d836b9245
Anava Jain <anavajain@Anavas-MBP.fritz.box>
2020-08-05 17:53:15 +0200

    Iterating over entries in Object

--- spring/src/main/java/com/anava/spring/Application.java
+++ spring/src/main/java/com/anava/spring/Application.java
@@ -1,9 +1,5 @@
 package com.anava.spring;
 
-import com.google.gson.Gson;
-import com.google.gson.stream.JsonReader;
-import net.minidev.json.JSONObject;
-import org.apache.tomcat.util.json.JSONParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -14,21 +10,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class Application {
 
-	private static final Logger log =  LoggerFactory.getLogger(Application.class);
-	Map map;
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	Map map;
 
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
@@ -41,7 +33,22 @@ public class Application {
 			Object object = restTemplate.getForObject(
 					"http://localhost:8080/users/", Object.class);
 			assert object != null;
-			//log.info(object.toString());
+			map = (Map) object;
+			for (Object obj : map.entrySet()) {
+				if(obj instanceof Map.Entry) {
+					if (((Map.Entry) obj).getValue() instanceof ArrayList) {
+						IntStream.range(0, ((ArrayList) ((Map.Entry) obj).getValue()).size()).filter(i -> ((ArrayList)
+								((Map.Entry) obj).getValue()).get(i) instanceof LinkedHashMap).forEach(i -> {
+							for (Object entry : ((LinkedHashMap)
+									((ArrayList) ((Map.Entry) obj).getValue()).get(i)).entrySet()) {
+								if (entry instanceof Map.Entry) {
+									//base level entries
+								}
+							}
+						});
+					}
+				}
+			}
 		};
 	}
 }
